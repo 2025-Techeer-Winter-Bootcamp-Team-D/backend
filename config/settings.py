edf2b77b25d16d3115cf9f31efbfba99a4c500ea
jang1224 <janghyuk2003x2003@gmail.com>
2026-01-13 01:54:08 +0900

    DJANGO_SECRET_KEY 를 .env로 이동

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(1#88xs9ypkmy&q*2fudx_z@43%$atbm%_435xt_^izxmbv&n_"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
