b24f314a168b29676fcf053d16b051f347bf381e
jang1224 <janghyuk2003x2003@gmail.com>
2026-01-14 00:09:21 +0900

    fix: urls 표시 중복 및 오류 수정

--- companies/urls.py
+++ companies/urls.py
@@ -4,5 +4,5 @@ from .views import get_company_info, get_company_rankings
 urlpatterns = [
     # 기업 상세 정보 주소
     path('companies/<str:stock_code>/', get_company_info, name='company_detail'),
-    path('rankings/', get_company_rankings, name='company_rankings'),
+    path('rankings/companies/', get_company_rankings, name='company_rankings'),
     ]
